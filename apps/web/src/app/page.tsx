26c447cc842d41a228de9a064250e716aadee1c5
Badsha Laskar <badshalaskar97@outlook.com>
2025-04-11 23:11:02 +0530

    PQK-UI card standard pushed

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,19 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import React from "react";
 
 export default function Home() {
   return (
     <React.Fragment>
-      <div className="">hihi</div>
+      <Card className="m-2">
+        <CardHeader></CardHeader>
+        <CardContent></CardContent>
+        <CardFooter></CardFooter>
+      </Card>
     </React.Fragment>
   );
 }
